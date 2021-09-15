f84175051ab6b96960ef8b0bc44ed7ea3fabb2cc
paulo.cintra <pauloons@gmail.com>
2021-09-15 17:40:29 -0300

    Segundo Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {PhotoService} from './photos/photo/photo.service';
+import {Photo} from './photos/photo/photo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+  export class AppComponent implements OnInit{
+
+  photos: any[] = [];
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos =>
+        this.photos = photos);
+  }
+}
