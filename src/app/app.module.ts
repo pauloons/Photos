5e2e9071a462c852399495d6ac763a087fb54a4c
pauloons <pauloons@gmail.com>
2021-09-28 00:38:21 -0300

    Melhorando apresentação da lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
-import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
-import {AppRoutingModule} from './app.routing.module';
-import {ErrorsModule} from './errors/errors.module';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoFormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
