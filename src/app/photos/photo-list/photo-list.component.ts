6ce76b56e504b99a23fb5b1ff7d5c7f65c18a3c9
pauloons <pauloons@gmail.com>
2021-10-04 23:50:37 -0300

    RxJS e seu Subject

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PhotoService } from '../photo/photo.service';
 import {Photo} from "../photo/photo";
+import {debounceTime} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'ap-photo-list',
@@ -13,12 +14,21 @@ export class PhotoListComponent implements OnInit {
 
  photos: Photo[] = [];
  filter:string[] =[];
+  debounce:Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
-    this.photos = this.activatedRoute.snapshot.data['photos']
+
+  ngOnInit() {
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy() {
+    this.debounce.unsubscribe();
   }
 }
